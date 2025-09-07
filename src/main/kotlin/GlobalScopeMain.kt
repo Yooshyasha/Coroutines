e169313b37488232c810cd22c3f32bc5b4215507
Yooshyasha <mejzikrejzi66@gmail.com>
2025-09-07 11:45:59 +0500

    feat

---
+++ src/main/kotlin/GlobalScopeMain.kt
@@ -0,0 +1,31 @@
+package com.yooshyasha
+
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@OptIn(DelicateCoroutinesApi::class)
+suspend fun main() {
+    val job = GlobalScope.launch {
+        delay(10)
+        println("Задача, завершённая с задержкой в 10 миллисекунд")
+    }
+
+    val job2 = GlobalScope.launch {
+        delay(1)
+        println("Задача, завершённая с задержкой в миллисекунду")
+    }
+
+    val job3 = GlobalScope.launch {
+        delay(1)
+        println("Задача, завершённая с задержкой в миллисекунду")
+    }
+
+    val job4 = GlobalScope.launch {
+        delay(1)
+        throw Exception("Задача, завершенная с ошибкой")
+    }
+
+    job.join()
+}
